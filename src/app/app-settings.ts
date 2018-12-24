9adb505d001453a00e9d9a3d5bcbcf8e32bcced4
Elite <elicloudh@gmail.com>
2018-12-24 14:35:34 +0000

    settings update with date format

--- src/app/app-settings.ts
+++ src/app/app-settings.ts
@@ -1,15 +1,32 @@
 import { Injectable } from '@angular/core';
 
+export class Settings {
+
+  constructor(public locale = 'en', public date_format = 'yyyy-MM-dd') {}
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class AppSettings {
 
-  public locale = 'en';
+  public settings: Settings;
 
   constructor() { }
 
-  setLocale(code) {
-    this.locale = code;
+  public init(settings: Settings) {
+    this.settings = settings;
+  }
+
+  public setLocale(locale) {
+    this.settings.locale = locale;
+  }
+
+  public get locale() {
+    return this.settings.locale;
+  }
+
+  public get date_format() {
+    return this.settings.date_format;
   }
 }
